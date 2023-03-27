d2c864bf1070ab8c6a683f9a65be479c1d81b2c3
Vikrant <vikrantgopewar@gmail.com>
2023-03-28 00:34:47 +0530

    added info.txt

--- src/main/java/com/example/repository/ProductRepo.java
+++ src/main/java/com/example/repository/ProductRepo.java
@@ -2,9 +2,7 @@ package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.entity.Product;
-
 import java.util.List;
 
 @Repository
