8a11fbe2eb97934084e7810743bd39cca27e4a35
vikrantgit97 <114150687+vikrantgit97@users.noreply.github.com>
2023-07-03 09:42:31 +0530

    Update AppConfig.java

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    
 /*    @Bean
     public OrderMapper orderMapper() {
         return new OrderMapper();
